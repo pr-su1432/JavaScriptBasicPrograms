d1befc7204e4de9efa7249489a876827de7115f7
[prasanna] <prasanna>
2022-08-17 17:12:31 +0530

    [prasanna]: Arrow functions

---
+++ js/ArrowFunctions.js
@@ -0,0 +1,43 @@
+
+//-----------let--------------
+ //let a;
+ //let b; 
+ //demo();
+ //function demo(a,b) {
+ //let mul = a*b;
+   //console.log(mul);
+   //return mul;
+ //}
+ //demo(50,70);
+
+
+
+//-----------var-------------
+// var x = 7;
+// var y = 8;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
+
+
+
+//----------const-------------
+// const x = 10;
+// const y = 6;
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply(x,y));
+
+
+//---------arrow function----------
+let multiply = (a, b) => {
+    let result = a * b;
+    return result;
+  }
+  
+  let result1 = multiply(5,7);
+  console.log(result1); 
