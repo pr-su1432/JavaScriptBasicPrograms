67a45d4de8cbc7b5ab573fb63eadc3d494f67da2
[prasanna] <prasanna>
2022-08-17 21:06:25 +0530

    [prasanna]: Es7 functions

---
+++ js/Es7.js
@@ -0,0 +1,17 @@
+//1.String_includes()---------------------------------
+ /*var text = "Hello Arjun, Welcome to World";
+ 
+ if (text.includes("abc"))
+ {
+     console.log("yes i am working in Bridgelabzs!!");
+ }
+ else
+{
+     console.log('NO i was not working in bridgelabzs!!');
+}*/
+
+
+
+//2. Index Operator  ** ------------------------
+console.log(12**10);
+console.log(Math.pow(5, 10)) 
