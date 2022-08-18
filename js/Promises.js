38d3f28f4d9060d5c878fa8d99cdcff82ab74051
[prasanna] <prasanna>
2022-08-18 22:59:54 +0530

    [prasanna]: Promises

---
+++ js/Promises.js
@@ -0,0 +1,23 @@
+//Promices----------------
+var Promises;
+new Promise((resolve, reject)=>{
+
+})
+.then(()=>{
+
+})
+.catch(()=>{
+
+});
+const xyz = () =>{
+    return new promise ((resolve,reject)=>{
+        let flag = true;
+        if (flag){
+            resolve("promise is resolve");
+        }else{
+            reject("promise is rejected");
+        }
+    });
+};
+console.log();
+
